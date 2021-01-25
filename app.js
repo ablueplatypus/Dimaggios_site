5b252a9bcb58a1610e7ce929149b50516ec66c13
Matt <mattdoering86@gmail.com>
2021-01-25 06:31:09 -0500

    updated dropdown menu & dinner menu

--- app.js
+++ app.js
@@ -1,22 +1,35 @@
-window.addEventListener('DOMContentLoaded', (e) => {
-	// console.log('page loaded')
-	let mlink = document.querySelector('.menus-link');
+window.addEventListener('DOMContentLoaded', () => {
+	console.log('page loaded');
 	let dropdownContent = document.querySelector('.dropdown-content');
 	let dropdown = document.querySelector('.dropdown');
-	let downIcon = document.querySelector('.fa-hide');
+	let caretIcon = document.querySelector('.fa-caret-down');
+	// console.log(screen.width)
+	// let regex = /Safari/i
+	// let browser = navigator.userAgent
+	// let found = browser.match(regex)
+	// console.log(found[0])
+	// if (found[0] === "Safari") {
+	// 	menuLink.style.display = "block";
+		
+	// }
 
-	if (window.innerWidth === 980) {
-		// mlink.href = "javascript:void(0)";
-		// downIcon.hidden = true;
-		window.addEventListener('onclick', (e) => {
-			// console.log('clicked menu', e)
+	if (screen.width <= 760) {
+		// dropdownContent.style.display === "none"
+			
+		dropdown.addEventListener('click',(e)=> {
+			// console.log('clicked menu',e)
 			if (dropdownContent.style.display === "none") {
 				dropdownContent.style.display = "block";
+				caretIcon.className = 'fa fa-caret-up';
+				// console.log('clicked')
 			} else {
 				dropdownContent.style.display = "none";
-			}
-		})
+				caretIcon.className = 'fa fa-caret-down'
+				// console.log('hidden')
 
+			}
+
+		})
 	}
 
 })
